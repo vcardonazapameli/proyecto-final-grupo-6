c1fa04d8b722e53f33731fa14336c3d451201fc3
gdeganis_meli <gaston.deganis@mercadolibre.com>
2025-01-28 11:43:18 -0300

    :construction: Added Seller Model

--- pkg/models/seller.go
+++ pkg/models/seller.go
@@ -1,4 +1,17 @@
 package models
 
 type Seller struct {
+	Id          int
+	Cid         int
+	CompanyName string
+	Address     string
+	Telephone   int
+}
+
+type SellerDoc struct {
+	Id          int    `json:"id"`
+	Cid         int    `json:"cid"`
+	CompanyName string `json:"company_name"`
+	Address     string `json:"address"`
+	Telephone   int    `json:"telephone"`
 }
