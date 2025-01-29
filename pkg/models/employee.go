2b18db0f2e2a84330a91aed6abe402f72c81aa4e
josemgarcia_meli <josemiguel.garcia@mercadolibre.com.co>
2025-01-29 10:05:20 -0500

    add employee models

--- pkg/models/employee.go
+++ pkg/models/employee.go
@@ -1,4 +1,21 @@
 package models
 
 type Employee struct {
+	Id                 int
+	EmployeeAttributes EmployeeAttributes
+}
+
+type EmployeeAttributes struct {
+	CardNumberID string
+	FirstName    string
+	LastName     string
+	WarehouseID  int
+}
+
+type EmployeeDoc struct {
+	Id           int    `json:"id"`
+	CardNumberID string `json:"card_number_id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	WarehouseID  int    `json:"warehouse_id"`
 }
