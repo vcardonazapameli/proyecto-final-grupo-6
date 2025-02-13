755fa43e11507417f72736e21a107a561aa37d52
arieleon_meli <arie_leon@mercadolibre.com>
2025-02-13 15:07:22 -0300

    :sparkles: create product-record (create)

--- cmd/server/application_default.go
+++ cmd/server/application_default.go
@@ -66,6 +66,7 @@ func (a *ServerChi) Run(cfg config.Config) (err error) {
 	routes.RegisterSectionRoutes(r)
 	routes.RegisterProductRoutes(r, database)
 	routes.RegisterBuyerRoutes(r)
+	routes.RegisterProductRecordRoutes(r, database)
 
 	// run server
 	err = http.ListenAndServe(a.serverAddress, r)
