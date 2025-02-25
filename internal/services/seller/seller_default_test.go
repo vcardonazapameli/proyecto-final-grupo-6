14fb0dedda7b2c239a7593374d85d1c6452233ce
gdeganis_meli <gaston.deganis@mercadolibre.com>
2025-02-25 15:24:11 -0300

    Add Service Creation Tests

--- internal/services/seller/seller_default_test.go
+++ internal/services/seller/seller_default_test.go
@@ -5,6 +5,7 @@ import (
 
 	repo "github.com/arieleon_meli/proyecto-final-grupo-6/internal/repositories/seller"
 	"github.com/arieleon_meli/proyecto-final-grupo-6/internal/services/seller"
+	"github.com/arieleon_meli/proyecto-final-grupo-6/internal/utils/customErrors"
 	"github.com/arieleon_meli/proyecto-final-grupo-6/pkg/models"
 	"github.com/stretchr/testify/assert"
 )
@@ -54,4 +55,72 @@ func TestReadSellers(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, expectedDoc, sDoc)
 	})
+}
+
+func TestCreateSellers(t *testing.T) {
+	t.Run("Create Seller Correctly", func(t *testing.T) {
+		// Arrange
+
+		mockRepo := new(repo.SellerRepositoryMock)
+		newSeller := models.NewSeller(1, 1000, "Company1", "Avenida Falsa 1", "11222333", 1)
+
+		mockRepo.On("Save", newSeller).Return(nil)
+
+		sv := seller.NewSellerServiceDefault(mockRepo)
+
+		expected := models.NewSellerDoc(1, 1000, "Company1", "Avenida Falsa 1", "11222333", 1)
+
+		// Act
+		res, err := sv.Create(*models.NewSellerDoc(1, 1000, "Company1", "Avenida Falsa 1", "11222333", 1))
+
+		// Assert
+		assert.NoError(t, err)
+		assert.Equal(t, *expected, res)
+	})
+
+	t.Run("Create Seller With Invalid attributes", func(t *testing.T) {
+		// Arrange
+		mockRepo := new(repo.SellerRepositoryMock)
+		sv := seller.NewSellerServiceDefault(mockRepo)
+
+		// Act
+		res, err := sv.Create(*models.NewSellerDoc(1, 0, "", "", "", 1))
+
+		// Assert
+		var valerr customErrors.ValidationError
+		assert.ErrorAs(t, err, &valerr)
+		assert.Empty(t, res)
+	})
+
+	t.Run("Create Seller With Invalid Locality ID", func(t *testing.T) {
+		// Arrange
+		mockRepo := new(repo.SellerRepositoryMock)
+		mockRepo.On("Save", models.NewSeller(1, 1000, "Company1", "Avenida Falsa 1", "11222333", -1)).Return(customErrors.ErrorNotFound)
+
+		sv := seller.NewSellerServiceDefault(mockRepo)
+
+		// Act
+		res, err := sv.Create(*models.NewSellerDoc(1, 1000, "Company1", "Avenida Falsa 1", "11222333", -1))
+
+		// Assert
+		assert.ErrorIs(t, err, customErrors.ErrorNotFound)
+		assert.Empty(t, res)
+	})
+
+	t.Run("Create Seller With Duplicated CID", func(t *testing.T) {
+
+		// Arrange
+		mockRepo := new(repo.SellerRepositoryMock)
+		mockRepo.On("Save", models.NewSeller(1, 1000, "Company1", "Avenida Falsa 1", "11222333", 1)).Return(customErrors.ErrorConflict)
+
+		sv := seller.NewSellerServiceDefault(mockRepo)
+
+		// Act
+		res, err := sv.Create(*models.NewSellerDoc(1, 1000, "Company1", "Avenida Falsa 1", "11222333", 1))
+
+		// Assert
+		assert.ErrorIs(t, err, customErrors.ErrorConflict)
+		assert.Empty(t, res)
+	})
+
 }
