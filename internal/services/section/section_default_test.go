38607700f83b9dc6f6e71fa244434015c5eda281
Anlly <vanessa.cardonazapata@mercadolibre.com.co>
2025-02-25 21:27:01 -0500

    ✨ feat: Added section service create test

--- internal/services/section/section_default_test.go
+++ internal/services/section/section_default_test.go
@@ -6,6 +6,7 @@ import (
 	pt "github.com/arieleon_meli/proyecto-final-grupo-6/internal/repositories/product_type"
 	st "github.com/arieleon_meli/proyecto-final-grupo-6/internal/repositories/section"
 	"github.com/arieleon_meli/proyecto-final-grupo-6/internal/services/section"
+	"github.com/arieleon_meli/proyecto-final-grupo-6/internal/utils/customErrors"
 	"github.com/arieleon_meli/proyecto-final-grupo-6/pkg/models"
 	"github.com/stretchr/testify/assert"
 )
@@ -28,21 +29,38 @@ var (
 	}
 )
 
+func setupMocks() (*st.SectionRepositoryMock, *pt.ProductTypeRepositoryMock, *section.SectionDefault) {
+	mockRepo := new(st.SectionRepositoryMock)
+	mockProductTypeRepo := new(pt.ProductTypeRepositoryMock)
+	sv := section.NewSectionDefault(mockRepo, mockProductTypeRepo)
+	return mockRepo, mockProductTypeRepo, sv.(*section.SectionDefault)
+}
+
 func TestCreateSection(t *testing.T) {
-	t.Run("case 1: create sections successfully", func(t *testing.T) {
+	t.Run("create_ok: create sections successfully", func(t *testing.T) {
 		// Arrange
-		mockRepo := new(st.SectionRepositoryMock)
-		mockRepo.On("Create", sectionAttributes).Return(newSection, nil)
-
-		mockProductTypeRepo := new(pt.ProductTypeRepositoryMock)
+		mockRepo, mockProductTypeRepo, sv := setupMocks()
 		mockProductTypeRepo.On("GetById", sectionAttributes.ProductTypeId).Return(&models.ProductTypeDocResponse{}, nil)
-
-		sv := section.NewSectionDefault(mockRepo, mockProductTypeRepo)
+		mockRepo.On("Create", sectionAttributes).Return(newSection, nil)
 		// Act
 		res, err := sv.Create(newSection)
 		// Assert
 		assert.NoError(t, err)
-		assert.NotNil(t, newSection)
+		assert.NotNil(t, res)
 		assert.Equal(t, newSection, res)
+	})
+
+	t.Run("create_conflict: section_number already exists", func(t *testing.T) {
+		// Arrange
+		mockRepo, mockProductTypeRepo, sv := setupMocks()
+		err := customErrors.ErrorConflict
+		mockProductTypeRepo.On("GetById", sectionAttributes.ProductTypeId).Return(&models.ProductTypeDocResponse{}, nil)
+		mockRepo.On("Create", sectionAttributes).Return(models.Section{}, err)
+		// Act
+		res, err := sv.Create(newSection)
+		// Assert
+		assert.Error(t, err)
+		assert.Equal(t, models.Section{}, res)
+
 	})
 }
