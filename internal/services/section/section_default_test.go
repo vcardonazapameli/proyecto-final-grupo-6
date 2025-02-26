dbcecbe0bfd409759284b1c8b4d0dd20c7c706f6
Anlly <vanessa.cardonazapata@mercadolibre.com.co>
2025-02-25 22:15:18 -0500

    ✨ feat: add tests for reading and updating sections

--- internal/services/section/section_default_test.go
+++ internal/services/section/section_default_test.go
@@ -27,6 +27,14 @@ var (
 		Id:                1,
 		SectionAttributes: sectionAttributes,
 	}
+
+	currentCapacity    = 15
+	currentTemperature = 22.0
+
+	updateSectionDto = models.UpdateSectionDto{
+		CurrentCapacity:    &currentCapacity,
+		CurrentTemperature: &currentTemperature,
+	}
 )
 
 func setupMocks() (*st.SectionRepositoryMock, *pt.ProductTypeRepositoryMock, *section.SectionDefault) {
@@ -62,5 +70,76 @@ func TestCreateSection(t *testing.T) {
 		assert.Error(t, err)
 		assert.Equal(t, models.Section{}, res)
 
+	})
+}
+
+func TestReadSection(t *testing.T) {
+	t.Run("find_all: read sections successfully", func(t *testing.T) {
+		// Arrange
+		mockRepo, _, sv := setupMocks()
+		sections := map[int]models.Section{
+			1: newSection,
+		}
+		mockRepo.On("GetAll").Return(sections, nil)
+		// Act
+		res, err := sv.GetAll()
+		// Assert
+		assert.NoError(t, err)
+		assert.Equal(t, sections, res)
+		assert.Len(t, res, 1)
+	})
+
+	t.Run("find_by_id: read section successfully", func(t *testing.T) {
+		// Arrange
+		mockRepo, _, sv := setupMocks()
+		mockRepo.On("GetByID", newSection.Id).Return(newSection, nil)
+		// Act
+		res, err := sv.GetByID(newSection.Id)
+		// Assert
+		assert.NoError(t, err)
+		assert.Equal(t, newSection, res)
+	})
+
+	t.Run("find_by_id_non_existent: section not found", func(t *testing.T) {
+		// Arrange
+		mockRepo, _, sv := setupMocks()
+		err := customErrors.ErrorNotFound
+		notExistentId := 9999
+		mockRepo.On("GetByID", notExistentId).Return(models.Section{}, err)
+		// Act
+		res, err := sv.GetByID(notExistentId)
+		// Assert
+		assert.ErrorIs(t, err, customErrors.ErrorNotFound)
+		assert.Empty(t, res)
+	})
+}
+
+func TestUpdateSection(t *testing.T) {
+	t.Run("update_existent: update section successfully", func(t *testing.T) {
+		// Arrange
+		mockRepo, mockProductTypeRepo, sv := setupMocks()
+		newSection.CurrentCapacity = currentCapacity
+		newSection.CurrentTemperature = currentTemperature
+		mockRepo.On("GetByID", newSection.Id).Return(newSection, nil)
+		mockProductTypeRepo.On("GetById", newSection.ProductTypeId).Return(&models.ProductTypeDocResponse{}, nil)
+		mockRepo.On("Update", newSection.Id, newSection).Return(newSection, nil)
+		// Act
+		res, err := sv.Update(newSection.Id, updateSectionDto)
+		// Assert
+		assert.NoError(t, err)
+		assert.Equal(t, newSection, res)
+	})
+
+	t.Run("update_non_existent: section not found", func(t *testing.T) {
+		// Arrange
+		mockRepo, _, sv := setupMocks()
+		nonExistentID := 9999
+		err := customErrors.ErrorNotFound
+		mockRepo.On("GetByID", nonExistentID).Return(models.Section{}, err)
+		// Act
+		res, err := sv.Update(nonExistentID, updateSectionDto)
+		// Assert
+		assert.ErrorIs(t, err, customErrors.ErrorNotFound)
+		assert.Empty(t, res)
 	})
 }
